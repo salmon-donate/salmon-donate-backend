dba82b2afc9e3d91f5cf3de68a4132e405b000af
KV <sharnet32@proton.me>
2024-12-08 23:48:52 +0100

    ref: add transactions

--- src/main/kotlin/com/github/chekist32/webhook/WebHookResource.kt
+++ src/main/kotlin/com/github/chekist32/webhook/WebHookResource.kt
@@ -1,6 +1,6 @@
 package com.github.chekist32.webhook
 
-import jakarta.inject.Named
+import jakarta.transaction.Transactional
 import jakarta.ws.rs.Consumes
 import jakarta.ws.rs.POST
 import jakarta.ws.rs.Path
@@ -8,14 +8,12 @@ import jakarta.ws.rs.Produces
 import jakarta.ws.rs.core.MediaType
 import jakarta.ws.rs.core.Response
 import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody
-import org.jooq.DSLContext
 
 @Path("/api/v1/webhook")
 class WebHookResource(
-    private val webHookHandler: KeycloakWebHookHandler,
-    @Named("dsl-sd")
-    private val dslSD: DSLContext
+    private val webHookHandler: KeycloakWebHookHandler
 ) {
+    @Transactional
     @POST
     @Path("/keycloak_event_webhook")
     @Consumes(MediaType.APPLICATION_JSON)
